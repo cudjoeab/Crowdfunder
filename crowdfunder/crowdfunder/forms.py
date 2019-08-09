f6ff0e893171517b57cd40244545c24ae2927c80
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-08-09 11:22:55 -0400

    signup/login/logout views and forms

---
+++ crowdfunder/crowdfunder/forms.py
@@ -0,0 +1,39 @@
+
+
+from django.forms import CharField, PasswordInput, Form
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class LoginForm(Form):
+    username = CharField(label="User Name", max_length=64)
+    password = CharField(widget=PasswordInput())
--- crowdfunder/crowdfunder/views.py
+++ crowdfunder/crowdfunder/views.py
@@ -14,13 +14,40 @@ def project_details(request):
     pass
 
 def login_view(request):
-    pass
+    if request.method == 'POST':
+        form = LoginForm (request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            pw = form.cleaned_data['password']
+            user = authenticate(username=username, password=pw)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/home')
+            else:
+                form.add_error('username', 'Login failed')
+    else:
+        form = LoginForm()
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_view(request):
-    pass
+    logout(request)
+    return HttpResponseRedirect('/home')
 
 def signup_view (request):
-    pass
+    if request.method == 'POST'
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(request, user)
+        return HttpResponseRedirect('/home')
+    else:
+        form = UserCreationForm()
+    html_response = render(request, 'signup.html', {'form': form})
+    return HttpResponse(html_response)
 
 def new_project(request):
     form = ProjectForm()
