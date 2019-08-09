2c5259140c5027d499a2bff4d2c09c23a8c8eef5
cudjoeab <cudjoeab@gmail.com>
2019-08-09 11:46:02 -0400

    imported new shortcuts

--- crowdfunder/crowdfunder/views.py
+++ crowdfunder/crowdfunder/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
